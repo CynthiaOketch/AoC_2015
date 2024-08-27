a18eec085dda3f8a166d86f9aa39ad1717243644
CynthiaOketch <cynthiaoketch6@gmail.com>
2024-08-27 12:39:57 +0300

    generated different functions for each part

--- day_1/main.go
+++ day_1/main.go
@@ -5,17 +5,27 @@ import (
 	"fmt"
 	"os"
 )
-
 func main() {
+	position, err := NotQuiteLisp2("input.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(position)
+}
+
+// NotQuiteLisp function calculates the position based on the content of the specified file
+func NotQuiteLisp(filename string) (int, error) {
 	countup := 0
 	countdown := 0
 	position := 0
-	file, err := os.Open("input.txt")
+
+	file, err := os.Open(filename)
 	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return
+		return 0, fmt.Errorf("error opening file: %w", err)
 	}
 	defer file.Close()
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		input := scanner.Text()
@@ -28,22 +38,46 @@ func main() {
 			position = countup - countdown
 		}
 	}
-	fmt.Println(position)
+
+	if err := scanner.Err(); err != nil {
+		return 0, fmt.Errorf("error reading file: %w", err)
+	}
+
+	return position, nil
 }
 
-// part2 solution
 
-// for scanner.Scan() {
-// 	input := scanner.Text()
-// 	for i, char := range input {
-// 		if char == '(' {
-// 			countup++
-// 		} else if char == ')' {
-// 			countdown++
-// 		}
-// 		if countdown > countup {
-// 			position = i + 1
-// 			break
-// 		}
-// 	}
-// }
+
+func NotQuiteLisp2(filename string) (int, error) {
+	countup := 0
+	countdown := 0
+	position := 0
+
+	file, err := os.Open(filename)
+	if err != nil {
+		return 0, fmt.Errorf("error opening file: %w", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		input := scanner.Text()
+		for i, char := range input {
+			if char == '(' {
+				countup++
+			} else if char == ')' {
+				countdown++
+			}
+			if countdown > countup {
+				position = i + 1
+				break
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return 0, fmt.Errorf("error reading file: %w", err)
+	}
+
+	return position, nil
+}
